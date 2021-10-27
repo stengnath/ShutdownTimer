d35b7cc79884a76f1f48c1b861fb57aaa987e989
Steffen <46190725+stengnath@users.noreply.github.com>
2021-10-27 09:30:48 +0200

    Initial Commit

---
+++ ShutdownTimer/ShutdownTimer/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace ShutdownTimer
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.cmdStartShutdown = new System.Windows.Forms.Button();
+      this.cmdNeustartPlanen = new System.Windows.Forms.Button();
+      this.trayicon = new System.Windows.Forms.NotifyIcon(this.components);
+      this.label1 = new System.Windows.Forms.Label();
+      this.cmbEinheit = new System.Windows.Forms.ComboBox();
+      this.cmbAnzahl = new System.Windows.Forms.ComboBox();
+      this.cmdAbort = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.lblStartTime = new System.Windows.Forms.Label();
+      this.timerCheck = new System.Windows.Forms.Timer(this.components);
+      this.label3 = new System.Windows.Forms.Label();
+      this.lblEndTime = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.lblRemaining = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // cmdStartShutdown
+      // 
+      this.cmdStartShutdown.BackColor = System.Drawing.Color.White;
+      this.cmdStartShutdown.Location = new System.Drawing.Point(12, 167);
+      this.cmdStartShutdown.Name = "cmdStartShutdown";
+      this.cmdStartShutdown.Size = new System.Drawing.Size(191, 61);
+      this.cmdStartShutdown.TabIndex = 0;
+      this.cmdStartShutdown.Text = "Herunterfahren planen";
+      this.cmdStartShutdown.UseVisualStyleBackColor = false;
+      this.cmdStartShutdown.Click += new System.EventHandler(this.cmdStartShutdown_Click);
+      // 
+      // cmdNeustartPlanen
+      // 
+      this.cmdNeustartPlanen.BackColor = System.Drawing.Color.White;
+      this.cmdNeustartPlanen.Location = new System.Drawing.Point(209, 167);
+      this.cmdNeustartPlanen.Name = "cmdNeustartPlanen";
+      this.cmdNeustartPlanen.Size = new System.Drawing.Size(161, 61);
+      this.cmdNeustartPlanen.TabIndex = 1;
+      this.cmdNeustartPlanen.Text = "Neustart planen";
+      this.cmdNeustartPlanen.UseVisualStyleBackColor = false;
+      this.cmdNeustartPlanen.Click += new System.EventHandler(this.cmdNeustartPlanen_Click);
+      // 
+      // trayicon
+      // 
+      this.trayicon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayicon.Icon")));
+      this.trayicon.Text = "Shutdown Timer";
+      this.trayicon.Visible = true;
+      this.trayicon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.trayicon_MouseClick);
+      this.trayicon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayicon_MouseDoubleClick);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 23);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(134, 20);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "Herunterfahren in";
+      // 
+      // cmbEinheit
+      // 
+      this.cmbEinheit.BackColor = System.Drawing.SystemColors.Window;
+      this.cmbEinheit.FormattingEnabled = true;
+      this.cmbEinheit.Items.AddRange(new object[] {
+            "Sekunden",
+            "Minuten",
+            "Stunden"});
+      this.cmbEinheit.Location = new System.Drawing.Point(376, 20);
+      this.cmbEinheit.Name = "cmbEinheit";
+      this.cmbEinheit.Size = new System.Drawing.Size(161, 28);
+      this.cmbEinheit.TabIndex = 3;
+      this.cmbEinheit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cmbEinheit_KeyPress);
+      this.cmbEinheit.KeyUp += new System.Windows.Forms.KeyEventHandler(this.cmbEinheit_KeyUp);
+      // 
+      // cmbAnzahl
+      // 
+      this.cmbAnzahl.BackColor = System.Drawing.SystemColors.Window;
+      this.cmbAnzahl.FormattingEnabled = true;
+      this.cmbAnzahl.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40",
+            "45",
+            "50",
+            "60"});
+      this.cmbAnzahl.Location = new System.Drawing.Point(209, 20);
+      this.cmbAnzahl.MaxLength = 2;
+      this.cmbAnzahl.Name = "cmbAnzahl";
+      this.cmbAnzahl.Size = new System.Drawing.Size(161, 28);
+      this.cmbAnzahl.TabIndex = 4;
+      this.cmbAnzahl.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cmbAnzahl_KeyPress);
+      // 
+      // cmdAbort
+      // 
+      this.cmdAbort.BackColor = System.Drawing.Color.White;
+      this.cmdAbort.Enabled = false;
+      this.cmdAbort.Location = new System.Drawing.Point(376, 167);
+      this.cmdAbort.Name = "cmdAbort";
+      this.cmdAbort.Size = new System.Drawing.Size(161, 61);
+      this.cmdAbort.TabIndex = 5;
+      this.cmdAbort.Text = "Abbrechen";
+      this.cmdAbort.UseVisualStyleBackColor = false;
+      this.cmdAbort.Click += new System.EventHandler(this.cmdAbort_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(12, 61);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(165, 20);
+      this.label2.TabIndex = 6;
+      this.label2.Text = "Planung gestartet am ";
+      // 
+      // lblStartTime
+      // 
+      this.lblStartTime.AutoSize = true;
+      this.lblStartTime.Location = new System.Drawing.Point(205, 61);
+      this.lblStartTime.Name = "lblStartTime";
+      this.lblStartTime.Size = new System.Drawing.Size(54, 20);
+      this.lblStartTime.TabIndex = 7;
+      this.lblStartTime.Text = "<Zeit>";
+      // 
+      // timerCheck
+      // 
+      this.timerCheck.Interval = 900;
+      this.timerCheck.Tick += new System.EventHandler(this.timerCheck_Tick);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(12, 92);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(138, 20);
+      this.label3.TabIndex = 8;
+      this.label3.Text = "Planung endet am";
+      // 
+      // lblEndTime
+      // 
+      this.lblEndTime.AutoSize = true;
+      this.lblEndTime.Location = new System.Drawing.Point(205, 92);
+      this.lblEndTime.Name = "lblEndTime";
+      this.lblEndTime.Size = new System.Drawing.Size(54, 20);
+      this.lblEndTime.TabIndex = 9;
+      this.lblEndTime.Text = "<Zeit>";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(12, 121);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(46, 20);
+      this.label4.TabIndex = 10;
+      this.label4.Text = "Noch";
+      // 
+      // lblRemaining
+      // 
+      this.lblRemaining.AutoSize = true;
+      this.lblRemaining.Location = new System.Drawing.Point(205, 121);
+      this.lblRemaining.Name = "lblRemaining";
+      this.lblRemaining.Size = new System.Drawing.Size(103, 20);
+      this.lblRemaining.TabIndex = 11;
+      this.lblRemaining.Text = "<Remaining>";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.White;
+      this.ClientSize = new System.Drawing.Size(553, 244);
+      this.Controls.Add(this.lblRemaining);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.lblEndTime);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.lblStartTime);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.cmdAbort);
+      this.Controls.Add(this.cmbAnzahl);
+      this.Controls.Add(this.cmbEinheit);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.cmdNeustartPlanen);
+      this.Controls.Add(this.cmdStartShutdown);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "ShutdownTimer";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button cmdStartShutdown;
+    private System.Windows.Forms.Button cmdNeustartPlanen;
+    private System.Windows.Forms.NotifyIcon trayicon;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.ComboBox cmbEinheit;
+    private System.Windows.Forms.ComboBox cmbAnzahl;
+    private System.Windows.Forms.Button cmdAbort;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label lblStartTime;
+    private System.Windows.Forms.Timer timerCheck;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label lblEndTime;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label lblRemaining;
+  }
+}
+
---
+++ ShutdownTimer/ShutdownTimer/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShutdownTimer
+{
+
+  public partial class Form1 : Form
+  {
+    public DateTime? _EndDate = null;
+    public DateTime? EndDate
+    {
+      get
+      {
+        return _EndDate;
+      }
+      set
+      {
+        _EndDate = value;
+        if (_EndDate == null)
+        {
+          lblEndTime.Text = "";
+          lblRemaining.Text = "";
+        }
+        else
+        {
+          lblEndTime.Text = _EndDate.ToString();
+        }
+      }
+    }
+
+    public DateTime? _startDate = null;
+    public DateTime? StartDate
+    {
+      get
+      {
+        return _startDate;
+      }
+      set
+      {
+        _startDate = value;
+        if (_startDate == null)
+        {
+          lblStartTime.Text = "";
+        }
+        else
+        {
+          lblStartTime.Text = _startDate.ToString();
+        }
+      }
+    }
+    private enum eProgramMode
+    {
+      none,
+      shutdown,
+      reboot
+    }
+    private eProgramMode _ProgramMode = eProgramMode.none;
+
+    private eProgramMode ProgramMode
+    {
+      get
+      {
+        return _ProgramMode;
+      }
+      set
+      {
+        _ProgramMode = value;
+        if (_ProgramMode == eProgramMode.none)
+        {
+          cmdAbort.Enabled = false;
+          cmdStartShutdown.Enabled = true;
+          cmdNeustartPlanen.Enabled = true;
+          cmbAnzahl.Enabled = true;
+          cmbEinheit.Enabled = true;
+        }
+        else
+        {
+          cmdAbort.Enabled = true;
+          cmdStartShutdown.Enabled = false;
+          cmdNeustartPlanen.Enabled = false;
+          cmbAnzahl.Enabled = false;
+          cmbEinheit.Enabled = false;
+        }
+      }
+    }
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      this.trayicon.Visible = true;
+      ContextMenuStrip traymenu = new ContextMenuStrip();
+      traymenu.Items.Add("Anzeigen", null, new EventHandler(Anzeigen));
+      traymenu.Items.Add("Timer Beenden", null, new EventHandler(Beenden));
+      trayicon.ContextMenuStrip = traymenu;
+      cmbAnzahl.SelectedIndex = 0;
+      cmbEinheit.SelectedIndex = 1;
+      StartDate = null;
+      EndDate = null;
+
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+
+      if (e.CloseReason == CloseReason.TaskManagerClosing || e.CloseReason == CloseReason.WindowsShutDown)
+      {
+        Environment.Exit(0);
+      }
+      else
+      {
+        e.Cancel = true;
+        HideWindow();
+      }
+    }
+
+    private void HideWindow()
+    {
+      this.ShowInTaskbar = false;
+      this.Visible = false;
+    }
+    private void ShowWindow()
+    {
+      this.ShowInTaskbar = true;
+      this.Visible = true;
+      this.BringToFront();
+    }
+
+    private void trayicon_MouseDoubleClick(object sender, MouseEventArgs e)
+    {
+      ShowWindow();
+    }
+
+    private void timerCheck_Tick(object sender, EventArgs e)
+    {
+      if (EndDate == null)
+      {
+        timerCheck.Stop();
+        return;
+      }
+      if (DateTime.Now >= EndDate)
+      {
+        ProcessStartInfo psi = null;
+        if (ProgramMode == eProgramMode.reboot)
+        {
+          psi = new ProcessStartInfo("shutdown", "/r /t 0");
+        }
+        else if (ProgramMode == eProgramMode.shutdown)
+        {
+          psi = new ProcessStartInfo("shutdown", "/s /t 0");
+        }
+        psi.CreateNoWindow = true;
+        psi.UseShellExecute = false;
+        Process.Start(psi);
+      }
+      else
+      {
+        var diff = EndDate.Value.Subtract(DateTime.Now);
+        if (diff.TotalMinutes <= 10 && (Convert.ToInt64(diff.TotalMinutes) % 2) == 0 && (Convert.ToInt64(diff.TotalSeconds) % 60) == 0)
+        {
+          ShowPushInfo(getNotifcationText(diff));
+        }
+        lblRemaining.Text = $"{diff.Hours.ToString("F0")} Stunden {diff.Minutes.ToString("F0")} Minuten {diff.Seconds.ToString("F0")} Sekunden";
+      }
+    }
+
+    private void ShowPushInfo(string Content)
+    {
+      Tulpep.NotificationWindow.PopupNotifier notification = new Tulpep.NotificationWindow.PopupNotifier();
+      notification.TitleText = "ACHTUNG - ShutdownTimer meldet";
+      notification.TitleFont = new Font("Tahoma", 12);
+      notification.ContentText = Content;
+      notification.ContentFont = new Font("Tahoma", 12);
+      notification.Image = ShutdownTimer.Properties.Resources.shutdown;
+      notification.ImageSize = new Size(70, 70);
+      notification.BodyColor = Color.White;
+      notification.Delay = 5000;
+      notification.Popup();
+    }
+
+    private string getNotifcationText(TimeSpan diff)
+    {
+      if (ProgramMode == eProgramMode.reboot)
+      {
+        if (diff.TotalSeconds <= 60)
+        {
+          return $"Das System wird in {diff.TotalSeconds.ToString("F0")} Sekunden neu gestartet";
+        }
+        return $"Das System wird in {diff.TotalMinutes.ToString("F0")} Minuten neu gestartet";
+      }
+      else if (ProgramMode == eProgramMode.shutdown)
+      {
+        if (diff.TotalSeconds <= 60)
+        {
+          return $"Das System wird in {diff.TotalSeconds.ToString("F0")} Sekunden heruntergefahren";
+        }
+        return $"Das System wird in {diff.TotalMinutes.ToString("F0")} Minuten heruntergefahren";
+      }
+      return "";
+    }
+    private void cmdAbort_Click(object sender, EventArgs e)
+    {
+      StartDate = null;
+      EndDate = null;
+      timerCheck.Stop();
+      ProgramMode = eProgramMode.none;
+    }
+
+    private void cmdStartShutdown_Click(object sender, EventArgs e)
+    {
+
+      EndDate = CalculateEndTime();
+      if (EndDate != null)
+      {
+        ProgramMode = eProgramMode.shutdown;
+        StartDate = DateTime.Now;
+        timerCheck.Start();
+        ShowPushInfo($"Herunterfahren wurde geplant.{Environment.NewLine}Shutdown wird ausgeführt um {EndDate.ToString()}");
+        HideWindow();
+      }
+
+    }
+
+    private void cmdNeustartPlanen_Click(object sender, EventArgs e)
+    {
+      EndDate = CalculateEndTime();
+      if (EndDate != null)
+      {
+        ProgramMode = eProgramMode.reboot;
+        StartDate = DateTime.Now;
+        timerCheck.Start();
+        ShowPushInfo($"Neustart wurde geplant.{Environment.NewLine}Reboot wird ausgeführt um {EndDate.ToString()}");
+        HideWindow();
+      }
+    }
+
+    private void trayicon_MouseClick(object sender, MouseEventArgs e)
+    {
+      if (e.Button == MouseButtons.Right)
+      {
+
+      }
+    }
+
+    private void Beenden(object sender, EventArgs e)
+    {
+      trayicon.Visible = false;
+      timerCheck.Stop();
+      Environment.Exit(0);
+    }
+
+    private void Anzeigen(object sender, EventArgs e)
+    {
+      ShowWindow();
+    }
+    private DateTime? CalculateEndTime()
+    {
+      try
+      {
+        if (cmbEinheit.SelectedItem.ToString() == "Sekunden")
+        {
+          return DateTime.Now.AddSeconds(Convert.ToDouble(cmbAnzahl.Text.ToString()));
+        }
+        else if (cmbEinheit.SelectedItem.ToString() == "Minuten")
+        {
+          return DateTime.Now.AddMinutes(Convert.ToDouble(cmbAnzahl.Text.ToString()));
+        }
+        else if (cmbEinheit.SelectedItem.ToString() == "Stunden")
+        {
+          return DateTime.Now.AddHours(Convert.ToDouble(cmbAnzahl.Text.ToString()));
+        }
+      }
+      catch (Exception)
+      {
+      }
+      return null;
+    }
+
+    private void cmbEinheit_KeyUp(object sender, KeyEventArgs e)
+    {
+      e.Handled = true;
+    }
+
+    private void cmbEinheit_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      e.Handled = true;
+    }
+
+    private void cmbAnzahl_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      if (Char.IsNumber(e.KeyChar) == false)
+      {
+        e.Handled = true;
+      }
+    }
+  }
+}
